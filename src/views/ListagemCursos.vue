bbcfba709e9ddcd02bdd55d4318013b6286148a1
EduardoLeao-system <carloseduardomeirelesleao@gmail.com>
2025-05-23 13:51:18 -0400

    adicinando listagem de cursos e estilizaçao

--- src/views/ListagemCursos.vue
+++ src/views/ListagemCursos.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="container mt-4">
     <h2>Lista de Cursos</h2>
+    <div class="row mb-4">
+      <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+        <div class="card border-info">
+          <div class="card-body text-center">
+            <img class="image-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" alt="Java Logo">
+            <h5 class="card-title">Java</h5>
+            <button class="btn btn-outline-secondary">Saiba mais</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+        <div class="card border-info">
+          <div class="card-body text-center">
+            <img class="image-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" alt="Docker Logo">
+            <h5 class="card-title">Docker</h5>
+            <button class="btn btn-outline-secondary">Saiba mais</button>
+          </div>
+        </div>
+      </div>
+      <!-- Adicione mais quadros aqui seguindo o mesmo padrão -->
+    </div>
+    <!-- Lista real de cursos -->
     <ul class="list-group">
       <li v-for="curso in cursos" :key="curso.id" class="list-group-item">
         <strong>{{ curso.nome }}</strong><br />
@@ -12,6 +34,7 @@
 
 <script>
 import api from '../services/api'
+import '/assets/listadeCursos.css'
 
 export default {
   data() {
